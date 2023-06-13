0c4eafe8d6e241401dd569fe4d8d8ee52f8a6004
sweetsk8er <cranefront@yandex.ru>
2023-06-13 13:40:03 +0300

    first step

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+		{
+        	test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
